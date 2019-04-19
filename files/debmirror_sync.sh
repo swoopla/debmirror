bbd4b5265c0232ad4a3278375ec598a6f6984638
swoopla <pvibet@gmail.com>
2019-04-19 14:14:17 +0200

    Fix "invalid number of arguments"
    Error when use command with env variable:

--- files/debmirror_sync.sh
+++ files/debmirror_sync.sh
@@ -1,6 +1,6 @@
 if test -e ${CONFDIR}/debmirror.conf; then
   ARGS="--config-file=${CONFDIR}/debmirror.conf"
 else
-  ARGS="/usr/bin/debmirror -v --method ${METHOD} -h ${SOURCE_SRV} -r ${SOURCE_DIR} --dist=${DIST} --section=${SECTION} --arch=${ARCH} --ignore-release-gpg --no-check-gpg"
+  ARGS="--method ${METHOD} -h ${SOURCE_SRV} -r ${SOURCE_DIR} --dist=${DIST} --section=${SECTION} --arch=${ARCH} --ignore-release-gpg --no-check-gpg"
 fi
-/usr/bin/debmirror -v ${ARGS} ${MIRRORDIR} # >> ${DEBUGFILE} 2>&1
+/usr/bin/debmirror -v "${ARGS}" "${MIRRORDIR}" # >> "${DEBUGFILE}" 2>&1
