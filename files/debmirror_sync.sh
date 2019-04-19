c207def5bbfeb7c0b388e0193e59a9cea09615dd
swoopla <pvibet@gmail.com>
2019-04-19 14:52:10 +0200

    Fix unknwon option
    with " many option are unkown

--- files/debmirror_sync.sh
+++ files/debmirror_sync.sh
@@ -3,4 +3,4 @@ if test -e ${CONFDIR}/debmirror.conf; then
 else
   ARGS="--method ${METHOD} -h ${SOURCE_SRV} -r ${SOURCE_DIR} --dist=${DIST} --section=${SECTION} --arch=${ARCH} --ignore-release-gpg --no-check-gpg"
 fi
-/usr/bin/debmirror -v "${ARGS}" "${MIRRORDIR}" # >> "${DEBUGFILE}" 2>&1
+/usr/bin/debmirror -v ${ARGS} ${MIRRORDIR} # >> "${DEBUGFILE}" 2>&1
